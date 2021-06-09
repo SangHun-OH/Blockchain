1466c99205a93580af8b47937b51494369fb3489
SangHun-OH <oshosh12@gmail.com>
2021-06-09 19:26:04 +0900

    IPFS_DApp

---
+++ ipfs_dApp/src/storehash.js
@@ -0,0 +1,12 @@
+/* eslint no-eval: 0 */
+import web3 from './web3';
+import StoreHash from './StoreHash.json';
+
+
+//console.log(address);
+
+
+//console.log(abi);
+
+
+export default new web3.eth.Contract(abi, address.replace(/"/g,""));
